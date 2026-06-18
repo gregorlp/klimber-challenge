dde5a6849fb185ee9ae6e435ac3472d4b6f0672e
gregorlp <gregor613@gmail.com>
2026-06-18 16:50:19 -0300

    feat: agregar idioma italiano

---
+++ DevelopmentChallenge.Data/Classes/Idiomas/IdiomaItaliano.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace DevelopmentChallenge.Data.Classes
+{
+    public class IdiomaItaliano : IIdioma
+    {
+        private static readonly Dictionary<string, string[]> _nombres =
+            new Dictionary<string, string[]>
+            {
+                { "cuadrado",  new[] { "Quadrato",  "Quadrati"  } },
+                { "circulo",   new[] { "Cerchio",   "Cerchi"    } },
+                { "triangulo", new[] { "Triangolo", "Triangoli" } },
+                { "trapecio",  new[] { "Trapezio",  "Trapezi"   } },
+            };
+
+        public string ListaVacia()       => "<h1>Elenco di forme vuoto!</h1>";
+        public string Encabezado()       => "<h1>Rapporto sulle forme</h1>";
+        public string TotalFormas(int n) => $"{n} forme";
+        public string LabelPerimetro()   => "Perimetro";
+
+        public string NombreFigura(string clave, int cantidad)
+            => cantidad == 1 ? _nombres[clave][0] : _nombres[clave][1];
+    }
+}
